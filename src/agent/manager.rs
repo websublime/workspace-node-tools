f9f5222173476011f03ef33a3d0cbe1722292780
Miguel Ramos <miguel.ramos@websublime.dev>
2024-07-02 23:14:58 +0100

    chore: tests for all enum agents

--- src/agent/manager.rs
+++ src/agent/manager.rs
@@ -59,13 +59,76 @@ impl Display for Agent {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs::{remove_file, File};
+
+    fn create_agent_file(path: &Path) -> File {
+        File::create(path).expect("File not created")
+    }
+
+    fn delete_agent_file(path: &Path) {
+        remove_file(path).expect("File not deleted");
+    }
+
+    #[test]
+    fn agent_for_npm_lock() {
+        let path = std::env::current_dir().expect("Current user home directory");
+        let npm_lock = path.join("package-lock.json");
+
+        create_agent_file(&npm_lock);
+
+        let agent = Agent::detect(&path);
+
+        assert_eq!(agent, Some(Agent::Npm));
+
+        delete_agent_file(&npm_lock);
+    }
+
+    #[test]
+    fn agent_for_yarn_lock() {
+        let path = std::env::current_dir().expect("Current user home directory");
+        let yarn_lock = path.join("yarn.lock");
+
+        create_agent_file(&yarn_lock);
+
+        let agent = Agent::detect(&path);
+
+        assert_eq!(agent, Some(Agent::Yarn));
+
+        delete_agent_file(&yarn_lock);
+    }
+
+    #[test]
+    fn agent_for_pnpm_lock() {
+        let path = std::env::current_dir().expect("Current user home directory");
+        let pnpm_lock = path.join("pnpm-lock.yaml");
+
+        create_agent_file(&pnpm_lock);
+
+        let agent = Agent::detect(&path);
+
+        assert_eq!(agent, Some(Agent::Pnpm));
+
+        delete_agent_file(&pnpm_lock);
+    }
+
+    #[test]
+    fn agent_for_bun_lock() {
+        let path = std::env::current_dir().expect("Current user home directory");
+        let bun_lock = path.join("bun.lockb");
+
+        create_agent_file(&bun_lock);
+
+        let agent = Agent::detect(&path);
+
+        assert_eq!(agent, Some(Agent::Bun));
+
+        delete_agent_file(&bun_lock);
+    }
 
     #[test]
     fn agent_not_present() {
         let path = std::env::current_dir().expect("Current user home directory");
         let agent = Agent::detect(&path);
-
-        dbg!(path);
 
         assert_eq!(agent, None);
     }

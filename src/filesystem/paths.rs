23305b4d0e551d209f12a587d4e4058d1013d236
Miguel Ramos <miguel.ramos@websublime.dev>
2024-07-03 07:07:24 +0100

    test: add tests for filesystem root detection

--- src/filesystem/paths.rs
+++ src/filesystem/paths.rs
@@ -35,3 +35,74 @@ fn walk_reverse_dir(path: &path::Path) -> Option<String> {
 
 	None
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::Path;
+    use std::fs::{remove_file, File};
+
+    fn create_agent_file(path: &Path) -> File {
+        File::create(path).expect("File not created")
+    }
+
+    fn delete_agent_file(path: &Path) {
+        remove_file(path).expect("File not deleted");
+    }
+
+    #[test]
+    fn npm_root_project() {
+        let path = std::env::current_dir().expect("Current user home directory");
+        let npm_lock = path.join("package-lock.json");
+
+        create_agent_file(&npm_lock);
+
+        let project_root = get_project_root_path();
+
+        assert_eq!(project_root, Some(path.to_str().unwrap().to_string()));
+
+        delete_agent_file(&npm_lock);
+    }
+
+    #[test]
+    fn yarn_root_project() {
+        let path = std::env::current_dir().expect("Current user home directory");
+        let yarn_lock = path.join("yarn.lock");
+
+        create_agent_file(&yarn_lock);
+
+        let project_root = get_project_root_path();
+
+        assert_eq!(project_root, Some(path.to_str().unwrap().to_string()));
+
+        delete_agent_file(&yarn_lock);
+    }
+
+    #[test]
+    fn pnpm_root_project() {
+        let path = std::env::current_dir().expect("Current user home directory");
+        let pnpm_lock = path.join("pnpm-lock.yaml");
+
+        create_agent_file(&pnpm_lock);
+
+        let project_root = get_project_root_path();
+
+        assert_eq!(project_root, Some(path.to_str().unwrap().to_string()));
+
+        delete_agent_file(&pnpm_lock);
+    }
+
+    #[test]
+    fn bun_root_project() {
+        let path = std::env::current_dir().expect("Current user home directory");
+        let bun_lock = path.join("bun.lockb");
+
+        create_agent_file(&bun_lock);
+
+        let project_root = get_project_root_path();
+
+        assert_eq!(project_root, Some(path.to_str().unwrap().to_string()));
+
+        delete_agent_file(&bun_lock);
+    }
+}

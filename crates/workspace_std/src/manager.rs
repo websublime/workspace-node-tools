88e5e48e891b9fe220e10f0ef812a1d99bce7f5a
Miguel Ramos <miguel.ramos@mercedes-benz.io>
2024-10-03 16:37:59 +0100

    chore: manager tests

--- crates/workspace_std/src/manager.rs
+++ crates/workspace_std/src/manager.rs
@@ -1,6 +1,6 @@
 use std::{
     collections::HashMap,
-    fmt::{Display, Formatter, Result},
+    fmt::{Display, Formatter, Result as StdResult},
     path::Path,
 };
 
@@ -25,7 +25,7 @@ impl From<String> for CorePackageManager {
 }
 
 impl Display for CorePackageManager {
-    fn fmt(&self, f: &mut Formatter) -> Result {
+    fn fmt(&self, f: &mut Formatter) -> StdResult {
         match self {
             Self::Npm => write!(f, "npm"),
             Self::Yarn => write!(f, "yarn"),
@@ -58,4 +58,74 @@ pub fn detect_package_manager(path: &Path) -> Option<CorePackageManager> {
     }
 
     None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test::MonorepoWorkspace;
+
+    #[test]
+    fn test_npm_manager() -> Result<(), std::io::Error> {
+        let monorepo = MonorepoWorkspace::new();
+        let root = monorepo.get_monorepo_root().clone();
+        monorepo.create_repository(&CorePackageManager::Npm)?;
+
+        let core_manager = detect_package_manager(root.as_path());
+        let manager = core_manager.unwrap();
+
+        assert_eq!(manager, CorePackageManager::Npm);
+
+        monorepo.delete_repository();
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_pnpm_manager() -> Result<(), std::io::Error> {
+        let monorepo = MonorepoWorkspace::new();
+        let root = monorepo.get_monorepo_root().clone();
+        monorepo.create_repository(&CorePackageManager::Pnpm)?;
+
+        let core_manager = detect_package_manager(root.as_path());
+        let manager = core_manager.unwrap();
+
+        assert_eq!(manager, CorePackageManager::Pnpm);
+
+        monorepo.delete_repository();
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_yarn_manager() -> Result<(), std::io::Error> {
+        let monorepo = MonorepoWorkspace::new();
+        let root = monorepo.get_monorepo_root().clone();
+        monorepo.create_repository(&CorePackageManager::Yarn)?;
+
+        let core_manager = detect_package_manager(root.as_path());
+        let manager = core_manager.unwrap();
+
+        assert_eq!(manager, CorePackageManager::Yarn);
+
+        monorepo.delete_repository();
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_bun_manager() -> Result<(), std::io::Error> {
+        let monorepo = MonorepoWorkspace::new();
+        let root = monorepo.get_monorepo_root().clone();
+        monorepo.create_repository(&CorePackageManager::Bun)?;
+
+        let core_manager = detect_package_manager(root.as_path());
+        let manager = core_manager.unwrap();
+
+        assert_eq!(manager, CorePackageManager::Bun);
+
+        monorepo.delete_repository();
+
+        Ok(())
+    }
 }

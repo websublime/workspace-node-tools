9a254991060cf9c2f14ac24373a86b7d255544f8
Miguel Ramos <miguel.ramos@mercedes-benz.io>
2024-10-03 15:41:42 +0100

    chore: remove old changes file

--- crates/workspace_std/src/changes.rs
+++ crates/workspace_std/src/changes.rs
@@ -357,8 +357,6 @@ mod tests {
         assert!(change_meta.deploy.contains(&"production".to_string()));
         assert_eq!(change_meta.pkgs.len(), 2);
 
-        assert!(true);
-
         monorepo.delete_repository();
 
         Ok(())

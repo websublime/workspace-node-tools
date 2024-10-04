a73fcfeb0850576bc3a75c480c27081ceb427b46
Miguel Ramos <miguel.ramos@mercedes-benz.io>
2024-10-04 15:30:03 +0100

    chore: fix release build action

--- packages/workspace-tools/src/binding.d.ts
+++ packages/workspace-tools/src/binding.d.ts
@@ -1,4 +1,6 @@
-import type { PackageManager } from './types/manager';
+import type { RepositoryCommit, RepositoryRemoteTags } from './types/repo';
+
+import type { PackageManager } from './types/enums';
 
 import type { WorkspaceConfig } from './types/config';
 
@@ -20,7 +22,55 @@ export declare function getChangesByPackage(package: string, branch: string, cwd
 
 export declare function getConfig(cwd?: string): Result<WorkspaceConfig>
 
+export declare function getProjectRootPath(cwd?: string | undefined | null): string | null
+
 export declare function initChanges(cwd?: string | undefined | null): Result<Changes>
 
+export declare function isVcsRepository(cwd: string): Result<boolean>
+
 export declare function removeChange(branch: string, cwd?: string): boolean
+
+export declare function repoAdd(filepath: string, cwd: string): Result<boolean>
+
+export declare function repoAddAll(cwd: string): Result<boolean>
+
+export declare function repoCheckout(branch: string, cwd: string): Result<boolean>
+
+export declare function repoCommit(cwd: string, message: string, body?: string | undefined | null, footer?: string | undefined | null): Result<boolean>
+
+export declare function repoConfig(username: string, email: string, cwd: string): Result<boolean>
+
+export declare function repoCreateBranch(branch: string, cwd: string): Result<boolean>
+
+export declare function repoCreateTag(tag: string, cwd: string, message?: string | undefined | null): Result<boolean>
+
+export declare function repoFetchAll(cwd: string, fetchTags?: boolean | undefined | null): Result<boolean>
+
+export declare function repoGetAllFilesChangedSinceBranch(cwd: string, packages: Array<string>, branch: string): Result<string[]>
+
+export declare function repoGetAllFilesChangedSinceSha(cwd: string, sha: string): Result<string[]>
+
+export declare function repoGetBranchFromCommit(sha: string, cwd: string): Result<string|null>
+
+export declare function repoGetCommitsSince(cwd: string, since?: string | undefined | null, relative?: string | undefined | null): Result<RepositoryCommit[]>
+
+export declare function repoGetCurrentBranch(cwd: string): Result<string|null>
+
+export declare function repoGetCurrentSha(cwd: string): Result<string>
+
+export declare function repoGetDivergedCommit(sha: string, cwd: string): Result<string>
+
+export declare function repoGetFirstSha(cwd: string, branch?: string | undefined | null): Result<string>
+
+export declare function repoGetPreviousSha(cwd: string): Result<string>
+
+export declare function repoGetTags(cwd: string, local?: boolean | undefined | null): Result<RepositoryRemoteTags[]>
+
+export declare function repoInit(initialBranch: string, username: string, email: string, cwd: string): Result<boolean>
+
+export declare function repoIsUnclean(cwd: string): Result<boolean>
+
+export declare function repoMerge(branch: string, cwd: string): Result<boolean>
+
+export declare function repoPush(cwd: string, followTags?: boolean | undefined | null): Result<boolean>
 
